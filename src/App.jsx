17e62de7b9a1bc0fe034fd6e7229fb345226f61e
Asad Atik <asadatik1995@gmail.com>
2025-07-01 18:47:12 +0600

    Creating your MY slice

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+
+  
+
+  return (
+    <>
+     <h1>COUNTER WITH REDUX</h1>
+      <button>Increment</button>
+      <p>0</p>
+      <button>Decrement</button>
+
+
+
+    </>
+  )
+}
+
+export default App
