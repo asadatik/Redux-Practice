d1592c92fd865870ffd7d90f63d119a4f802b508
Asad Atik <asadatik1995@gmail.com>
2025-07-02 09:22:56 +0600

    use dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { useDispatch } from "react-redux"
-
+import {increment , decrement} from "./Redux/feature/counter/counterSlice"
 
 
 function App() {
 
   const dispatch = useDispatch() ;
 
+    const handleIncrement = () => {
+      dispatch(increment())
+    }
+
     
+    const handleDecrement = () => {
+      dispatch(decrement())
+    }
+
 
   return (
     <>
      <h1>COUNTER WITH REDUX</h1>
-      <button>Increment</button>
+      <button  onClick={handleIncrement}  >Increment</button>
       <p>0</p>
-      <button>Decrement</button>
+      <button  onClick={handleDecrement} >  Decrement</button>
 
 
 
